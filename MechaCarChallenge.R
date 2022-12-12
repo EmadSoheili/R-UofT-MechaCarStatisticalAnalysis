cc0b9d629e6a487e8520146113a4555187275967
EmadSoheili <emadsoheili@Emads-MacBook-Pro.local>
2022-12-11 19:46:01 -0500

    Part 4 completed

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,6 +3,8 @@ library(dplyr)
 # Part 1
 mpg_df <- read.csv('MechaCar_mpg.csv')
 
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mpg_df)
+
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mpg_df))
 
 summary(lm(mpg ~ vehicle_length + ground_clearance, mpg_df))
@@ -23,3 +25,7 @@ lot2 <- subset(susp_df, Manufacturing_Lot == 'Lot2')
 lot3 <- subset(susp_df, Manufacturing_Lot == 'Lot3')
 
 t.test (lot1$PSI, mu=1500)
+
+t.test (lot2$PSI, mu=1500)
+
+t.test (lot3$PSI, mu=1500)
